5cbcb89c40f1b5f24fa5992a59a7f1ec21d39223
macielnassar <macielnassarm@gmail.com>
2026-04-22 18:17:54 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Importar Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+// TU CONFIG (REEMPLAZA CON LA TUYA)
 const firebaseConfig = {
-  apiKey: "AIzaSyBqJ9bSb2gwJMF64T7fD_2PBBHIe3VRNg4",
-  authDomain: "tickets-system-baa0f.firebaseapp.com",
-  projectId: "tickets-system-baa0f",
-  storageBucket: "tickets-system-baa0f.firebasestorage.app",
-  messagingSenderId: "617421342489",
-  appId: "1:617421342489:web:31ce139de74b0edf1b6788"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_ID",
+  appId: "TU_APP_ID"
 };
 
-// Initialize Firebase
+// Inicializar
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Exportar
+export { db };
