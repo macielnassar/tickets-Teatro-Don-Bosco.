fe93bc67658d84753d91efdb39daf951b562444a
macielnassar <macielnassarm@gmail.com>
2026-04-22 00:02:32 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -19,3 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp(firebaseConfig);
