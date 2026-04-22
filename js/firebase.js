9021fb8333f7ec34ce9c47ae663c46699bf8de31
macielnassar <macielnassarm@gmail.com>
2026-04-22 18:21:12 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 
 // TU CONFIG (REEMPLAZA CON LA TUYA)
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBqJ9bSb2gwJMF64T7fD_2PBBHIe3VRNg4",
+  authDomain: "tickets-system-baa0f.firebaseapp.com",
+  projectId: "tickets-system-baa0f",
+  storageBucket: "tickets-system-baa0f.firebasestorage.app",
+  messagingSenderId: "617421342489",
+  appId: "1:617421342489:web:31ce139de74b0edf1b6788"
 };
 
 // Inicializar
