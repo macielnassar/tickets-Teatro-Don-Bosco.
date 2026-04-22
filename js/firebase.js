4e00f72af54d054b43187d8c2ec2caf4324fd2f3
macielnassar <macielnassarm@gmail.com>
2026-04-22 01:16:08 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,18 @@
-// Replace with your Firebase project configuration later
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
-import {
-  getAuth
-} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
-import {
-  getFirestore
-} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: "AIzaSyCtOT318VIQPeIwNKNHekvU2HQiw3oVHv4",
+  authDomain: "don-bosco-tickets.firebaseapp.com",
+  projectId: "don-bosco-tickets",
+  storageBucket: "don-bosco-tickets.firebasestorage.app",
+  messagingSenderId: "943287220988",
+  appId: "1:943287220988:web:d0baafef8cc17a7a457579"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-import { initializeApp } from "firebase/app";
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-const app = initializeApp(firebaseConfig);
+export { db, auth };
